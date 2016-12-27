ab32b32ebf329e49f0d7968d1ae66e5252ee6138
sakuke <sakuke@sakukes-iMac.local>
2016-12-27 08:03:49 -0800

    first commit

---
+++ Keeper/MainTabBarController.swift
@@ -0,0 +1,22 @@
+//
+//  MainTabBarController.swift
+//  Keeper
+//
+//  Created by sakuke on 12/27/16.
+//  Copyright © 2016 Quang. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        self.tabBar.tintColor = UIColor.green
+    }
+    
+    override func didReceiveMemoryWarning() {
+        
+    }
+}
---
+++ Keeper/ThirdViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ThirdViewController.swift
+//  Keeper
+//
+//  Created by sakuke on 12/26/16.
+//  Copyright © 2016 Quang. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        
+    }
+
+}
